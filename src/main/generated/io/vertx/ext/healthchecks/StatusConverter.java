3a2f6654dc6251a4e88d94b6536d2b75d27bea96
Julien Viet <julien@julienviet.com>
2019-07-25 17:55:12 +0200

    Releasing 4.0.0-milestone1

--- src/main/generated/io/vertx/ext/healthchecks/StatusConverter.java
+++ src/main/generated/io/vertx/ext/healthchecks/StatusConverter.java
@@ -4,12 +4,21 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import io.vertx.core.spi.json.JsonCodec;
 
 /**
- * Converter for {@link io.vertx.ext.healthchecks.Status}.
+ * Converter and Codec for {@link io.vertx.ext.healthchecks.Status}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.ext.healthchecks.Status} original class using Vert.x codegen.
  */
-public class StatusConverter {
+public class StatusConverter implements JsonCodec<Status, JsonObject> {
+
+  public static final StatusConverter INSTANCE = new StatusConverter();
+
+  @Override public JsonObject encode(Status value) { return (value != null) ? value.toJson() : null; }
+
+  @Override public Status decode(JsonObject value) { return (value != null) ? new Status(value) : null; }
+
+  @Override public Class<Status> getTargetClass() { return Status.class; }
 
   public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, Status obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
