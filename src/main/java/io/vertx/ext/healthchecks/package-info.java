837743ef4a39daec83211cdd08e41a248e2b37ee
Thomas Segismont <tsegismont@gmail.com>
2023-03-03 16:23:19 +0100

    Deprecate callbacks
    See vert-x3/issues#612
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/ext/healthchecks/package-info.java
+++ src/main/java/io/vertx/ext/healthchecks/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "vertx-health-checks", groupPackage = "io.vertx")
+@ModuleGen(name = "vertx-health-checks", groupPackage = "io.vertx", checkCallbackDeprecation = true)
 package io.vertx.ext.healthchecks;
 
 import io.vertx.codegen.annotations.ModuleGen;
