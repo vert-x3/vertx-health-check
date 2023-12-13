359e965e5c7c57459f77f2d1c59b49e134a60ee7
Julien Viet <julien@julienviet.com>
2023-12-13 16:58:59 +0100

    Set version to 4.5.2-SNAPSHOT

--- src/main/generated/io/vertx/ext/healthchecks/StatusConverter.java
+++ src/main/generated/io/vertx/ext/healthchecks/StatusConverter.java
@@ -17,7 +17,7 @@ public class StatusConverter {
   private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
   private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
-  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, Status obj) {
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, Status obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "data":
@@ -39,11 +39,11 @@ public class StatusConverter {
     }
   }
 
-  public static void toJson(Status obj, JsonObject json) {
+   static void toJson(Status obj, JsonObject json) {
     toJson(obj, json.getMap());
   }
 
-  public static void toJson(Status obj, java.util.Map<String, Object> json) {
+   static void toJson(Status obj, java.util.Map<String, Object> json) {
     if (obj.getData() != null) {
       json.put("data", obj.getData());
     }
